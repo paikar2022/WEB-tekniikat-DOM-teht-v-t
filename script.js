fdabe0e0e31ae293dd6fba45e5ab4fee799c7f0c
paikar2022 <n1kapa00@students.oamk.fi>
2022-04-20 10:11:33 +0300

    Fixed some typos and stuff

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// a. Print the tag name of node with id ”content”.-> eli tulostaa DIV consolesse
-
+// a. Print the tag name of node with id ”content”.-> eli tulostaa DIV consolessa
 let elem = document.getElementById("content");
 elem = document.querySelector("#content");
-console.log(elem.tagName); //tätä herjasi vaikka niinku opella, kunnes laitoin scriptin tunnukseen defer.
-//vauhtoehto olis ollu siirtää js linkki sinne alas.
+console.log(elem.tagName); 
 
 // b. Print the tag name of first node with class name ”basic” -> eli tulostaa P consolessa
 elem = document.querySelector(".basic");
@@ -17,16 +15,14 @@ for (elem of liElems) {
 }
 
 // d. Print all node names of body descendants -> eli kaikki bodyn jälkeläiset, tagit, tekstit jne consoleen.
-let bodyDesc = document.querySelectorAll("body *"); //tässä tähti merkkaa jälkeläisiä
+let bodyDesc = document.querySelectorAll("body *"); //tässä tähti tarkopittaa jälkeläisiä
 for (elem of bodyDesc) {   
-console.log(elem.nodeName);//tää oli vähän epäselvä, yritin käyttää letissä eka tuota 
-//nodenamea nimenä mutta ei toiminut. -> Ilmeisesti koska nodename täytyy olla tuossa 
-// toisessa kohassa propertyna joten siksi?
+console.log(elem.nodeName);// nodeName täytyy olla tuossa toisessa kohassa propertyna joten ei voi käyttää nimenä let:ssa
 }
 
 // e. Print text content of all p elements that are descendants of article element.
 // Eli tulostaa consoleen kaikkien articlen p-jälkeläisten tekstisisällöt.
-let articlePDesc = document.querySelectorAll("article p"); //eli articlen p-jälkeläiset
+let articlePDesc = document.querySelectorAll("article p"); //articlen p-jälkeläiset
 for (elem of articlePDesc) {   
 console.log(elem.textContent);
 }
@@ -35,7 +31,7 @@ console.log(elem.textContent);
 //kuukausien tilalle Johnin nimen tai mitä muuta halutaan.
 //liElems = document.getElementsByTagName("li");
 // tai let liElems = document.querySelectorAll("li");
-//tää hakuhan on jo oikeestaan tuolla ylempänä tehty c-kohdassa, eli haettiin li- elementit
+//tämä haku kommenteiksi, sehän on jo oikeestaan tuolla ylempänä tehty c-kohdassa, eli haettiin li- elementit
 for (elem of liElems) {
     elem.textContent = "John Doe";
 }
